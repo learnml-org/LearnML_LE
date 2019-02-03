e476ec9860a1c2d6eb646e9589803499d382f8b7
kmc7468 <kmc7468@naver.com>
2019-02-03 21:19:58 +0900

    링크 오류 수정

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,7 +1,11 @@
 #include <lml_le/engine.hpp>
 
+#include <limits>
+
 namespace lml_le
 {
+	std::uint64_t version = std::numeric_limits<std::uint64_t>::max();
+
 	std::uint32_t initialize(std::uint64_t version) noexcept
 	{
 		lml_le::version = version;
